070026162696afd42c3b3b783b55fc46068670de
Arghya Biswas <arghya@ialoy.com>
2025-05-11 13:50:51 +0530

    Add LDI LDM HLT and SAV instruction
    Signed-off-by: Arghya Biswas <arghya@ialoy.com>

--- Compiler/Compiler.py
+++ Compiler/Compiler.py
@@ -1,5 +1,9 @@
 import argparse
 import os
+import hexdump
+
+MAX_VAL_8_BIT     = 255
+MAX_ADDRESS_8_BIT = 255
 
 def isInt(s):
     try:
@@ -30,49 +34,72 @@ class Compiler:
             print(f"Not able to open the {assemblyFile}!!!")
             exit(-1)
 
-        self.assemblyFile = assemblyFile
-        self.assemblyMain = assembly
-        self.outFile      = outFile
-        self.silent       = silent
-        self.tagDict      = dict()
-        self.maxLength    = 0
-        self.addressIndex = 0
-        self.logBuffer    = []
-        self.registerList = ['A', 'B', 'C', 'D']
-        self.registerDict = {
+        self.assemblyFile       = assemblyFile
+        self.assemblyMain       = assembly
+        self.outFile            = outFile
+        self.silent             = silent
+        self.tagDict            = dict()
+        self.binArr             = bytearray()
+        self.sourceMaxLength    = 0
+        self.tagMaxLength       = 0
+        self.addressIndex       = 0
+        self.logBuffer          = []
+        self.registerList       = ['A', 'B', 'C', 'D']
+        self.registerDict       = {
             'A': 0b00,
             'B': 0b01,
             'C': 0b10,
             'D': 0b11
         }
         self.instructionDict = {
-            'ADD': 0b0000,
-            'MOV': 0b0001,
-            'SUB': 0b0100,
-            'INC': 0b1000,
-            'DEC': 0b1100,
-            'JMP': 0b1101
+            'NOP': 0b0000,
+            'ADD': 0b0001,
+            'SUB': 0b0010,
+            'INC': 0b0011,
+            'DEC': 0b0100,
+            'MOV': 0b0101,
+            'LDI': 0b0110,
+            'LDM': 0b0111,
+            'JMP': 0b1000,
+            'SAV': 0b1001,
+            'HLT': 0b1111
         }
 
         self.instructionSizeDict = {
+            'NOP': 1,
             'ADD': 1,
-            'MOV': 1,
             'SUB': 1,
             'INC': 1,
             'DEC': 1,
-            'JMP': 2
+            'MOV': 1,
+            'LDI': 2,
+            'LDM': 2,
+            'JMP': 2,
+            'SAV': 2,
+            'HLT': 1
         }
 
         self.preProcess()
         self.compile()
 
         if not self.silent:
+            print()
             for item in self.logBuffer:
                 print(item)
 
             print("\nTAGs")
             for key, value in self.tagDict.items():
-                print(f"0x{value:08X}: {key}")
+                print(f"0x{value:04X}: {key}")
+
+            print("\n\n  Binary  ", end = "")
+            for col in range(0x8):
+                print(f"{col:02X} ", end = "")
+            print(" ", end = "")
+            for col in range(0x8, 0x10):
+                print(f"{col:02X} ", end = "")
+            print("\n          ------------------------------------------------")
+
+            hexdump.hexdump(self.binArr)
 
 
     def preProcess(self):
@@ -89,8 +116,8 @@ class Compiler:
         # get mex size of lines
         for line in self.assemblyLine:
             length = len(line)
-            if length > self.maxLength:
-                self.maxLength = length
+            if length > self.sourceMaxLength:
+                self.sourceMaxLength = length
 
         # write intermediate file for debug
         if not self.silent:
@@ -104,17 +131,27 @@ class Compiler:
 
     def printCompiledLine(self, line, value, value2 = None):
         binary  = f"{value:08b}"
+        tag = next((key for key, value in self.tagDict.items() if value == self.addressIndex), None)
+        if tag == None:
+            tag = " "*self.tagMaxLength
+        else:
+            tag = f"{tag:{self.tagMaxLength}}"
+
         if value2 != None:
             binary2 = f"{value2:08b}"
-            self.logBuffer.append(f"0x{self.addressIndex:08X}: '{line:{self.maxLength}}'; Code: {binary[:4]}_{binary[4:]} {binary2[:4]}_{binary2[4:]} : 0x{value:02X} 0x{value2:02X}")
+            self.logBuffer.append(f"{tag} 0x{self.addressIndex:04X}: {line:{self.sourceMaxLength}} | Code: {binary[:4]}_{binary[4:]} {binary2[:4]}_{binary2[4:]} // 0x{value:02X} 0x{value2:02X}")
         else:
-            self.logBuffer.append(f"0x{self.addressIndex:08X}: '{line:{self.maxLength}}'; Code: {binary[:4]}_{binary[4:]}           : 0x{value:02X}")
+            self.logBuffer.append(f"{tag} 0x{self.addressIndex:04X}: {line:{self.sourceMaxLength}} | Code: {binary[:4]}_{binary[4:]}           // 0x{value:02X}")
 
 
     def compile(self):
-        def errorPrint(index):
+        def errorPrint(index, error = None):
             errorLine = self.assemblyMain[index]
-            raise Exception(f"'{errorLine}' at line no {index + 1} is not able to compile!!!")
+            if error != None:
+                raise Exception(f"'{errorLine}' at line no {index + 1} is not able to compile!!!\nERRROR: {error}")
+            else:
+                raise Exception(f"'{errorLine}' at line no {index + 1} is not able to compile!!!")
+
 
         ## Parsing logic the tags first
         self.addressIndex = 0
@@ -128,13 +165,14 @@ class Compiler:
             if len(splitData) == 1 and line[-1] == ":":
                 tag = line[:-1].upper()
                 self.tagDict[tag] = self.addressIndex
+                if len(tag) > self.tagMaxLength:
+                    self.tagMaxLength = len(tag)
                 continue
 
             self.addressIndex += self.instructionSizeDict[opcode]
 
 
         ## Parsing for each line
-        binArr = bytearray()
         self.addressIndex = 0
         for index, line in enumerate(self.assemblyLine):
             if not line:
@@ -168,7 +206,7 @@ class Compiler:
                 if not self.silent:
                     self.printCompiledLine(line, bitVal)
 
-                binArr.append(bitVal)
+                self.binArr.append(bitVal)
                 self.addressIndex += 1
 
 
@@ -189,7 +227,7 @@ class Compiler:
                 if not self.silent:
                     self.printCompiledLine(line, bitVal)
 
-                binArr.append(bitVal)
+                self.binArr.append(bitVal)
                 self.addressIndex += 1
 
 
@@ -215,11 +253,97 @@ class Compiler:
                 if not self.silent:
                     self.printCompiledLine(line, bitVal, address)
 
-                binArr.append(bitVal)
+                self.binArr.append(bitVal)
                 self.addressIndex += 1
 
-                binArr.append(address)
+                self.binArr.append(address)
                 self.addressIndex += 1
+
+
+            ## Parse LDI command
+            elif opcode == "LDI":
+                if payloadLen != 2:
+                    errorPrint(index, f"2 payload expected!!, but found {payloadLen}")
+
+                destReg = payloadList[0]
+                if destReg not in self.registerList:
+                        errorPrint(index, "Destination register not found!!")
+
+                immediateVal = payloadList[1]
+                if isInt(immediateVal):
+                    immediateVal = int(immediateVal)
+                elif isHex(immediateVal):
+                    immediateVal = int(immediateVal, 16)
+                else:
+                    errorPrint(index, f"{immediateVal} is not a int or hex value!!")
+
+                if immediateVal > MAX_VAL_8_BIT:
+                    errorPrint(index, "Max value limit cross!!")
+
+                bitVal = bitVal | self.registerDict[destReg]
+
+                bitVal = bitVal << 4
+                bitVal = bitVal | self.instructionDict[opcode]
+
+                if not self.silent:
+                    self.printCompiledLine(line, bitVal, immediateVal)
+
+                self.binArr.append(bitVal)
+                self.addressIndex += 1
+
+                self.binArr.append(immediateVal)
+                self.addressIndex += 1
+
+
+            ## Parse LDM, SAV command
+            elif opcode == "LDM" or opcode == "SAV":
+                if payloadLen != 2:
+                    errorPrint(index, f"2 payload expected!! but found {payloadLen}")
+
+                destReg = payloadList[0]
+                if destReg not in self.registerList:
+                        errorPrint(index, "Destination register not found!!")
+
+                memAddress = payloadList[1]
+                if isInt(memAddress):
+                    memAddress = int(memAddress)
+                elif isHex(memAddress):
+                    memAddress = int(memAddress, 16)
+                else:
+                    errorPrint(index, f"{memAddress} is not a int or hex value!!")
+
+                if memAddress > MAX_ADDRESS_8_BIT:
+                    errorPrint(index, "Max address limit cross!!")
+
+                bitVal = bitVal | self.registerDict[destReg]
+
+                bitVal = bitVal << 4
+                bitVal = bitVal | self.instructionDict[opcode]
+
+                if not self.silent:
+                    self.printCompiledLine(line, bitVal, memAddress)
+
+                self.binArr.append(bitVal)
+                self.addressIndex += 1
+
+                self.binArr.append(memAddress)
+                self.addressIndex += 1
+
+
+            ## Parse HLT commands
+            elif opcode == "HLT":
+                if payloadLen != 0:
+                    errorPrint(index, f"No payload expected!! but found {payloadLen}")
+
+                bitVal = bitVal << 4
+                bitVal = bitVal | self.instructionDict[opcode]
+
+                if not self.silent:
+                    self.printCompiledLine(line, bitVal)
+
+                self.binArr.append(bitVal)
+                self.addressIndex += 1
+
 
             # Looks like a TAG
             elif opcode[-1] == ":":
@@ -228,8 +352,9 @@ class Compiler:
                 errorPrint(index)
 
         f = open(self.outFile, 'wb')
-        f.write(binArr)
+        f.write(self.binArr)
         f.close()
+
 
 def main():
     parser = argparse.ArgumentParser()
